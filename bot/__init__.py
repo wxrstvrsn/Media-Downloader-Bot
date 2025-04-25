202e46604ffd26b1e813d0a5d2ca9bb21f92bf12
wxrstvrsn <prestige.2014.xx@gmail.com>
2025-04-26 01:40:15 +0300

    preparations for deployment

---
+++ bot/__init__.py
@@ -0,0 +1,6 @@
+﻿# bot/__init__.py
+
+"""
+MediaDownloader Bot
+Created for downloading YouTube videos via Telegram bot.
+"""
--- bot/config.py
+++ bot/config.py
@@ -1,34 +1,14 @@
 ﻿import os
-from dotenv import load_dotenv
 
-load_dotenv()
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+DOWNLOAD_DIR = os.path.join(os.path.dirname(__file__), "downloads")
+PUBLIC_URL = os.getenv("PUBLIC_URL")  # Если хочешь оставить ссылки на скачку
+MAX_FILE_SIZE = 50 * 1024 * 1024  # 50 MB
 
-# Токен Telegram бота
-BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
-
-# Хост для aiohttp (слушаем все интерфейсы)
-BIND_HOST = '0.0.0.0'
-PORT = 8000
-
-# Публичный адрес, по которому можно скачать файл (меняй на IP или домен при надобности)
-PUBLIC_URL = os.getenv('PUBLIC_URL', f'http://localhost:{PORT}')
-
-# Папка загрузок
-DOWNLOAD_DIR = os.path.join(os.path.dirname(__file__), 'downloads')
-os.makedirs(DOWNLOAD_DIR, exist_ok=True)
-
-# URL для формирования ссылок
-DOWNLOAD_BASE_URL = f'{PUBLIC_URL}/downloads'
-
-# Опции yt-dlp
 YDL_OPTS = {
-    'outtmpl': 'downloads/%(title)s.%(ext)s',
+    'outtmpl': DOWNLOAD_DIR + '/%(title)s.%(ext)s',
     'format': 'bestvideo+bestaudio/best',
     'merge_output_format': 'mp4',
     'quiet': True,
     'no_warnings': True,
 }
-
-
-# Максимальный размер файла
-MAX_FILE_SIZE = 5000 * 1024 * 1024  # 50 MB
