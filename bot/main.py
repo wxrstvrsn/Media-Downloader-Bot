ec510044b696c3884ddb3afbc3ad6fdc69237aec
wxrstvrsn <prestige.2014.xx@gmail.com>
2025-04-26 17:34:22 +0300

    v0.1.7-beta rev. 42 |-- Still fighting w format buttons

--- bot/main.py
+++ bot/main.py
@@ -105,7 +105,7 @@ async def format_chosen(callback: CallbackQuery):
 
     msg = await callback.message.answer("⏳ Загружаю видео...")
 
-    filename = await download_video(url, itag)
+    filename = download_video(url, itag)
     if filename:
         file_url = f"{PUBLIC_URL}/downloads/{filename}"
         kb = InlineKeyboardMarkup(inline_keyboard=[[
